db88596bab900b97221646243cc13a821c60cc31
pedro <pedroramirezneira@gmail.com>
2024-05-09 19:29:11 -0300

    Addition of UI repository, main class update

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -8,8 +8,18 @@ plugins {
 group = "org.example"
 version = "unspecified"
 
+val githubUser: Any = project.findProperty("github.user") ?: System.getenv("GITHUB_USER")
+val githubToken: Any = project.findProperty("github.token") ?: System.getenv("GITHUB_TOKEN")
+
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/austral-ingsis/chess-ui")
+        credentials {
+            username = githubUser as String
+            password = githubToken as String
+        }
+    }
 }
 
 dependencies {
@@ -32,7 +42,7 @@ dependencies {
 }
 
 application {
-    mainClass.set("com.example.ApplicationKt")
+    mainClass.set("com.mediaversetv.chess.ApplicationKt")
 }
 
 tasks.test {
